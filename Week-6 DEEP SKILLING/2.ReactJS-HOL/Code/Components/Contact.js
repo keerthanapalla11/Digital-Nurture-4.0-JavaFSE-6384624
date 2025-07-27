9dfb6eebd1fa3a950ca06f451ffbced26d2c2af5
keerthanapalla11 <keerthanap1105@gmail.com>
2025-07-27 16:48:36 +0530

    WEEK-6 uploaded

---
+++ Week-6 DEEP SKILLING/2.ReactJS-HOL/Code/Components/Contact.js
@@ -0,0 +1,14 @@
+// src/Components/Contact.js
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to the Contact page of the Student Management Portal</h2>
+      </div>
+    );
+  }
+}
+
+export default Contact;
